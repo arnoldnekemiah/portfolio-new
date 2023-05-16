60590a1e59b49a29ed0ddd429a1cade6dca851ce
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-05-16 07:54:51 +0300

    Fixed menu

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
+//mobile menu
 let mobile_menu = document.getElementById('menu-popup');
 let openMenuButton = document.getElementById("btn-menu");
-let closeMenuButton = document.getElementById('close');
+let closeMenuButton = document.querySelector('.close');
+let links = document.querySelectorAll('.nav-link a');
 
-function openMenu(){
-    mobile_menu.classList.toggle("menu-popup");
-    mobile_menu.style.display = 'flex';
+function openMenu() {
+  mobile_menu.style.display = 'flex';
 }
 
 openMenuButton.addEventListener('click', openMenu);
 
-function closeMenu(){
-    closeMenuButton.style.display = 'none';
-    mobile_menu.style.display = 'none';
-
+function closeMenu() {
+  mobile_menu.style.display = 'none';
 }
 
 closeMenuButton.addEventListener('click', closeMenu);
-const links = document.querySelectorAll('.nav-link a');
-links.forEach();
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobile_menu.style.display = 'none';
+  });
+});
